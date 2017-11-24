1598b20846e554c6c4d5cde2248ff6c349ab7f6f
Chris Gardner <chris@blackmarble.co.uk>
2017-11-24 11:46:35 +0000

    Initial work on unit tests for xEventLog in same location as other DSC Resources.

---
+++ Tests/Unit/EventLog.Tests.ps1
@@ -0,0 +1,118 @@
+﻿    <#
+    .NOTES
+
+#>
+
+Import-Module $PSScriptRoot\..\..\DSCResources\MSFT_xEventLog\MSFT_xEventLog.psm1 -Prefix EventLog -Force
+
+Describe 'EventLog Get-TargetResource'{
+
+    Mock -CommandName Write-Verbose -MockWith {}
+
+    Mock Get-EventLog -ModuleName MSFT_xEventLog {
+        $properties = @{
+            Source = 'Application1'
+        }
+
+        Write-Output (New-Object -TypeName PSObject -Property $properties)
+    }
+
+    $results = Get-EventLogTargetResource 'Application'
+
+    It 'Should return an hashtable'{
+        $results.GetType().Name | Should Be 'HashTable'
+    }
+
+    It 'Should return a Hashtable LogName is Pester'{
+        $results.LogName = 'Pester'
+    }
+
+    It 'Should return a Hashatable with the Source is Application1'{
+        $results.Source | Should Be 'Application1'
+    }
+}
+
+Describe 'EventLog Test-TargetResource'{
+
+    Mock -CommandName Write-Verbose -MockWith {}
+
+    Mock -CommandName Get-TargetResource -ModuleName MSFT_xEventLog -MockWith {
+        $properties = @{
+            Source = 'Application1'
+        }
+
+        Write-Output (New-Object -TypeName PSObject -Property $properties)
+    } -ParameterFilter {$LogName -eq 'Pester'}
+
+    Mock -CommandName Get-TargetResource -ModuleName MSFT_xEventLog -MockWith {
+        $properties = @{
+            Source = @('Application1','Application2')
+        }
+
+        Write-Output (New-Object -TypeName PSObject -Property $properties)
+    } -ParameterFilter {$LogName -eq 'ArrayOfSources'}
+
+    Mock -CommandName Get-TargetResource -ModuleName MSFT_xEventLog -MockWith {
+        Throw "Event Log $LogName doesn't exist"
+    } -ParameterFilter {$LogName -eq 'NotExisting'}
+
+    $params = @{
+        LogName = 'Pester'
+        Source = 'Application1'
+    }
+
+
+    It 'should return true when all properties match'{
+        $testResults = Test-EventLogTargetResource @params
+        $testResults | Should Be $True
+    }
+
+    It 'should return false when Source does not match'{
+        $testResults = Test-EventLogTargetResource -LogName 'Pester' -Source 'Application2'
+        $testResults | Should Be $False
+    }
+
+    It 'should return false when an array of Sources does not match'{
+        $testResults = Test-EventLogTargetResource -LogName 'ArrayOfSources' -Source @('Application2','Application3')
+        $testResults | Should Be $False
+    }
+
+    It 'should return false when LogName does not exist'{
+        $testResults = Test-EventLogTargetResource -LogName 'NotExisting' -Source 'Application2'
+        $testResults | Should Be $False
+    }
+
+    It 'Should call Get-TargetResource' {
+        Assert-MockCalled Get-TargetResource -ModuleName MSFT_xEventLog -Exactly -Times 4
+    }
+}
+
+Describe 'EventLog Set-TargetResource'{
+    Mock -CommandName Write-EventLog -MockWith {}
+    Mock -CommandName Write-Verbose -MockWith {}
+    Mock -CommandName New-EventLog -MockWith { $true }
+
+
+    Context 'When set is called and event log does not exist' {
+        Mock -CommandName New-EventLog -MockWith { $true }
+        Mock -CommandName Get-TargetResource -ModuleName MSFT_xEventLog -MockWith { Throw }
+
+        It 'Should create the event log' {
+            Mock -CommandName New-EventLog -MockWith { $true }
+            Set-EventLogTargetResource -LogName 'Pester' -Source 'SetTargetResource'
+            Assert-MockCalled -CommandName New-EventLog -Exactly -Times 1
+            Assert-MockCalled -CommandName Get-TargetResource -Exactly -Times 1
+        }
+
+        It 'Should throw an error if it fails to create the new event log'{
+            Mock -CommandName New-EventLog -MockWith {Throw}
+            { Set-EventLogTargetResource -LogName 'Pester' -Source 'SetTargetResource' } | Should Throw
+        }
+    }
+
+    Context "When EventLog exists but Sources don't match"{
+
+
+    }
+
+}
